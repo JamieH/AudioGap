756df7710d7447cb4fba66b223d87f0429027c68
Jamie Hankins <jamiehankin@gmail.com>
2014-04-06 19:56:01 +0100

    Add a quick UDP Listener also make debug launch both client and server

--- AudioGapServer/Program.cs
+++ AudioGapServer/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,8 +10,25 @@ namespace AudioGapServer
 {
     class Program
     {
+        private const int listenPort = 11000;
+
         static void Main(string[] args)
         {
+            UdpClient server = new UdpClient(listenPort);
+            IPEndPoint serverEndPoint = new IPEndPoint(IPAddress.Any, listenPort);
+
+            byte[] data;
+
+
+
+            while (true)
+            {
+                data = server.Receive(ref serverEndPoint);
+                Console.WriteLine("Message from: {0}", serverEndPoint.ToString());
+            }
+
+            server.Close();
+
         }
     }
 }
