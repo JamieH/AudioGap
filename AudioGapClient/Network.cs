a9b4fb17a6417624db96197dcc8dd212a7840629
Jamie Hankins <jamiehankin@gmail.com>
2014-04-06 02:39:16 +0100

    Back to NAudio

--- AudioGapClient/Network.cs
+++ AudioGapClient/Network.cs
@@ -5,19 +5,31 @@ using System.Net;
 using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
+using AudioGap;
+using NAudio.CoreAudioApi;
+using NAudio.Wave;
 
 namespace AudioGapClient
 {
     class Network
     {
-        static UdpClient udpClient = new UdpClient();
-        public static void connect(IPEndPoint endpoint)
-        {
+        static Codec codec = new Codec();
 
+        static UdpClient udpClient = new UdpClient();
+        public static void connect(IPEndPoint endpoint, MMDevice device)
+        {
+            IWaveIn waveIn = new WasapiLoopbackCapture(device);
+           
+            waveIn.DataAvailable += SendData;
+            waveIn.StartRecording();
 
             udpClient.Connect(endpoint);
         }
 
-
+        static void SendData(object sender, WaveInEventArgs e)
+        {
+            byte[] encoded = codec.Encode(e.Buffer, 0, e.BytesRecorded);
+            udpClient.Send(encoded, encoded.Length);
+        }
     }
 }
--- AudioGapClient/Program.cs
+++ AudioGapClient/Program.cs
@@ -4,7 +4,6 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using Un4seen.Bass;
 
 namespace AudioGapClient
 {
@@ -14,8 +13,6 @@ namespace AudioGapClient
         [STAThread]
         static void Main(string[] args)
         {
-            BassNet.Registration("swkauker@yahoo.com", "2X2832371834322");
-
             Application.EnableVisualStyles();
             Application.Run(new UI()); // or whatever
         }
